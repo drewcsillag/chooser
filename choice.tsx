b58f16167502d258827669ae6d83dd033657d395
Drew Csillag <drew@thecsillags.com>
2020-05-16 14:19:50 -0400

    working tsx

--- choice.tsx
+++ choice.tsx
@@ -48,6 +48,13 @@ export class Chooser {
     const ind = this.choose_index(l.length);
     return l[ind];
   }
+
+  public pick<T>(l: T[]): T {
+    const ind = this.choose_index(l.length);
+    const ret = l[ind];
+    l.splice(ind, 1);
+    return ret;
+  }
 }
 
 function test_binary_counter(c: Chooser) {
@@ -55,5 +62,52 @@ function test_binary_counter(c: Chooser) {
   console.log(l);
 }
 
+function test_solve_magic_square(c: Chooser, counterbox: number[]) {
+  const left = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const square = [];
+  counterbox[1]++;
+
+  square.push(c.pick(left));
+  square.push(c.pick(left));
+  square.push(c.pick(left));
+  if (square[0] + square[1] + square[2] !== 15) {
+    return;
+  }
+
+  square.push(c.pick(left));
+  square.push(c.pick(left));
+  square.push(c.pick(left));
+  if (square[3] + square[4] + square[5] !== 15) {
+    return;
+  }
+
+  square.push(c.pick(left));
+  if (square[0] + square[3] + square[6] !== 15 || square[2] + square[4] + square[6] !== 15) {
+    return;
+  }
+
+  square.push(c.pick(left));
+  if (square[1] + square[4] + square[7] !== 15) {
+    return;
+  }
+
+  square.push(c.pick(left));
+  if (
+    square[6] + square[7] + square[8] !== 15 ||
+    square[2] + square[5] + square[8] !== 15 ||
+    square[0] + square[4] + square[8] !== 15
+  ) {
+    return;
+  }
+
+  console.log(square.slice(0, 3));
+  console.log(square.slice(3, 6));
+  console.log(square.slice(6, 9));
+  console.log('');
+  counterbox[0] += 1;
+}
 const runner = new ChoiceRunner();
+const counterbox = [0, 0];
+runner.run((c: Chooser) => test_solve_magic_square(c, counterbox));
+console.log('solutions, total executions:', counterbox);
 runner.run(test_binary_counter);
