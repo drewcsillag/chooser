291ddc7084ab50c16e035e06a1ec83b65ff6a2b0
Drew Csillag <drew@thecsillags.com>
2020-05-16 12:37:29 -0400

    magic square example

--- choice.py
+++ choice.py
@@ -5,9 +5,8 @@ class Chooser(object):
         self.prechosen = prechosen
         self.index = 0
         self.newChoices = []
-        # print("CI prechose %r" % (prechosen,))
 
-    def choose(self, *args):
+    def choose(self, args):
         if self.index < len(self.prechosen):
             ret = self.prechosen[self.index]
             self.index += 1
@@ -18,35 +17,61 @@ class Chooser(object):
                 self.runner.executions.append(
                     execution
                 )
-                # print("New exec %r" % (execution,))
             self.newChoices.append(args[0])
             return args[0]
+
+    def stop(self):
+        self.runner.stop()
 
 class ChoiceRunner(object):
     def __init__(self):
         self.executions = []
-        self.maxlen = 0
 
     def run(self, fn):
         chooser = Chooser(self, [])
         fn(chooser)
         while self.executions:
-            self.maxlen = max(self.maxlen, len(self.executions))
-
             execution = self.executions[-1]
             chooser = Chooser(self, execution)
             self.executions = self.executions[:-1]
             fn(chooser)
+    def stop(self):
+        self.executions = []
 
+def test_solve_magic_square(chooser, counterbox):
+    left = [1,2,3,4,5,6,7,8,9]
+    square = []
+    for i in range(9):
+        c = chooser.choose(left)
+        left.remove(c)
+        square.append(c)
+        if i == 2 and square[0] + square[1] + square[2] != 15:
+            return
+        elif i == 5 and square[3] + square[4] + square[5] != 15:
+            return
 
-def f(c):
+    if i == 8 and square[6] + square[7] + square[8] != 15:
+        return
+    elif square[0] + square[3] + square[6] != 15:
+        return
+    elif square[1] + square[4] + square[7] != 15:
+        return
+    elif square[2] + square[5] + square[8] != 15:
+        return
+    elif square[0] + square[4] + square[8] != 15:
+        return
+    elif square[2] + square[4] + square[6] != 15:
+        return
 
-    c1 = c.choose(0, 1)
-    c2 = c.choose(0, 1)
-    c3 = c.choose(0, 1)
-    c4 = c.choose(0, 1)
-    print("%r %r %r %r" % (c1, c2, c3, c4))
+    print(square[0:3])
+    print(square[3:6])
+    print(square[6:9])
+    print()
+    counterbox[0] += 1
+    # chooser.stop() # to stop at first solution
 
-runner=ChoiceRunner()
-runner.run(f)
-print("maxlen %r" %(runner.maxlen,))
+if __name__ == '__main__':
+    runner=ChoiceRunner()
+    counterbox = [0]
+    runner.run(lambda chooser: test_solve_magic_square(chooser, counterbox))
+    print("solutions", counterbox[0])
