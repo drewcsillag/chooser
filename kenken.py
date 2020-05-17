0be10464d0e860415581936594bd8b2f893fbb34
Drew Csillag <drew@thecsillags.com>
2020-05-17 17:18:32 -0400

    blacked

--- kenken.py
+++ kenken.py
@@ -1,6 +1,7 @@
 import choice3
 
 from typing import List
+
 # C0  C1  C2  C3
 # C4  C5  C6  C7
 # C8  C9  C10 C11
@@ -35,8 +36,11 @@ def all_but(candidates, *used):
 
 
 ONE_TO_FOUR = [1, 2, 3, 4]
+
+
 def addChoice(row: List[int], c: choice3.Chooser, *used: int):
     row.append(c.choose(all_but(ONE_TO_FOUR, *used)))
+
 
 def kenken(c: choice3.Chooser, box):
     box[0] += 1
@@ -46,15 +50,15 @@ def kenken(c: choice3.Chooser, box):
 
     try:
         # 2nd row
-        addChoice(row, c, 4, row[0]) # C4
-        addChoice(row, c, 4, row[1], row[4])  #C5
+        addChoice(row, c, 4, row[0])  # C4
+        addChoice(row, c, 4, row[1], row[4])  # C5
         if row[0] * row[1] * row[5] != 16:
             return
-        addChoice(row, c, 4, row[2], row[4], row[5]) #C6
+        addChoice(row, c, 4, row[2], row[4], row[5])  # C6
         if row[2] + row[3] + row[6] != 7:
             return
 
-        row.append(4) #C7
+        row.append(4)  # C7
 
         # 3rd row
         addChoice(row, c, row[0], row[4])  # C8
@@ -62,7 +66,7 @@ def kenken(c: choice3.Chooser, box):
             return
         addChoice(row, c, row[1], row[5], row[8])  # C9
         addChoice(row, c, row[2], row[6], row[8], row[9])  # C10
-        addChoice(row, c, row[3], row[7], row[8], row[9], row[10]) # C11
+        addChoice(row, c, row[3], row[7], row[8], row[9], row[10])  # C11
         if not divcheck(row[10], row[11], 2):
             return
 
@@ -72,7 +76,7 @@ def kenken(c: choice3.Chooser, box):
         if row[9] * row[12] * row[13] != 12:
             return
         addChoice(row, c, row[2], row[6], row[10], row[12], row[13])  # C14
-        addChoice(row, c, row[3], row[7], row[11], row[12], row[13], row[14]) # C15
+        addChoice(row, c, row[3], row[7], row[11], row[12], row[13], row[14])  # C15
         if not divcheck(row[14], row[15], 2):
             return
 
