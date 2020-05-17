0f1eef11ea6cc573dea5b70c6b47d7d900f42c3c
Drew Csillag <drew@thecsillags.com>
2020-05-17 19:10:33 -0400

    canned other versions of choice

--- python/choice.py
+++ python/choice.py
@@ -1,31 +1,50 @@
+from typing import Callable, List, TypeVar
+
+T = TypeVar("T")
+
+
 class Chooser(object):
-    def __init__(self, runner, prechosen):
+    def __init__(self, runner: 'ChoiceRunner', prechosen: List[int]):
         self.runner = runner
         self.prechosen = prechosen
         self.index = 0
-        self.newChoices = []
+        self.newChoices: List[int] = []
 
-    def choose(self, args):
+    def choose_index(self, numArgs: int) -> int:
         if self.index < len(self.prechosen):
-            ret = self.prechosen[self.index]
+            retind = self.prechosen[self.index]
             self.index += 1
-            return ret
-        else:
-            for i in args[1:]:
-                execution = self.prechosen + self.newChoices + [i]
-                self.runner.executions.append(execution)
-            self.newChoices.append(args[0])
-            return args[0]
+            return retind
 
-    def stop(self):
-        self.runner.stop()
+        for i in range(1, numArgs):
+            execution = self.prechosen + self.newChoices + [i]
+            self.runner.executions.append(execution)
+        self.newChoices.append(0)
+        return 0
+
+    def choose(self, args: List[T]) -> T:
+        try:
+            index = self.choose_index(len(args))
+            return args[index]
+        except IndexError:
+            print("trying index %d of %r" % (index, args))
+            raise
+
+    def pick(self, l: List[T]) -> T:
+        c = self.choose_index(len(l))
+        ret = l[c]
+        del l[c]
+        return ret
+
+    def stop(self) -> None:
+        self.runner.executions[:] = []
 
 
 class ChoiceRunner(object):
-    def __init__(self):
-        self.executions = []
+    def __init__(self) -> None:
+        self.executions: List[List[int]] = []
 
-    def run(self, fn):
+    def run(self, fn: Callable[[Chooser], None]) -> None:
         chooser = Chooser(self, [])
         fn(chooser)
         while self.executions:
@@ -34,33 +53,37 @@ class ChoiceRunner(object):
             self.executions = self.executions[:-1]
             fn(chooser)
 
-    def stop(self):
-        self.executions = []
 
-
-def test_solve_magic_square(chooser, counterbox):
+def test_solve_magic_square(c: Chooser, counterbox: List[int]) -> None:
     left = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     square = []
     counterbox[1] += 1
 
-    for i in range(9):
-        c = chooser.choose(left)
-        left.remove(c)
-        square.append(c)
-
+    square.append(c.pick(left))  # 0
+    square.append(c.pick(left))  # 1
+    square.append(c.pick(left))  # 2
     if square[0] + square[1] + square[2] != 15:
         return
-    elif square[3] + square[4] + square[5] != 15:
+
+    square.append(c.pick(left))  # 3
+    square.append(c.pick(left))  # 4
+    square.append(c.pick(left))  # 5
+    if square[3] + square[4] + square[5] != 15:
         return
-    elif (
+
+    square.append(c.pick(left))  # 6
+    if (
         square[0] + square[3] + square[6] != 15
         or square[2] + square[4] + square[6] != 15
     ):
         return
-    elif square[1] + square[4] + square[7] != 15:
+
+    square.append(c.pick(left))  # 7
+    if square[1] + square[4] + square[7] != 15:
         return
 
-    elif square[6] + square[7] + square[8] != 15:
+    square.append(c.pick(left))  # 8
+    if square[6] + square[7] + square[8] != 15:
         return
     elif square[2] + square[5] + square[8] != 15:
         return
@@ -75,8 +98,13 @@ def test_solve_magic_square(chooser, counterbox):
     # chooser.stop() # to stop at first solution
 
 
+def test_binary_counter(c: Chooser) -> None:
+    print([c.choose([0, 1]) for i in range(3)])
+
+
 if __name__ == "__main__":
     runner = ChoiceRunner()
     counterbox = [0, 0]
     runner.run(lambda chooser: test_solve_magic_square(chooser, counterbox))
-    print("solutions", counterbox)
+    print("solutions, total executions:", counterbox)
+    runner.run(test_binary_counter)
